d633af0cbe7d8a852a0a3162217b6cde01df1a0a
Mary Waithera <njihiamary11@gmail.com>
2020-02-14 16:37:47 +0300

    Move table to new html file

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function sname(){
+  var sname=document.getElementById("sname").value;
+  if (sname===" "){
+  return false;
+  }; 
+  
+}
+alert("Please Enter Your Name");
+function year(){
+  var birthYear=document.getElementById("birthyear").value;
+  if (birthYear===" "){
+    alert("Please enter the year you were born")
+    return false;
+}
+}
+function month(){
+  var mm=document.getElementById("birthday");
+  if (mm===0){
+    alert("Please select your birth month")
+  }
+}
+function letters(){
+  var sname=document.getElementById("sname").value;
+}
+// function day(){
+//   var dd=document.getElementById("birthday");
+//   if (dd==="0" &&)
+// }
